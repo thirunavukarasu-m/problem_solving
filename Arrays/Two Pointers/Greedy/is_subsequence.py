a1c0173709a6bb056ab5007209826a0dabe360bd
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-07-30 14:51:01 +0530

    Added is Subsequence

---
+++ Arrays/Two Pointers/Greedy/is_subsequence.py
@@ -0,0 +1,49 @@
+'''
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+
+https://leetcode.com/problems/is-subsequence/description/
+'''
+
+def isSubsequence(s, t):
+    if s == '':
+        return True
+    if len(s) > len(t):
+        return False
+    l, r = 0,0
+    
+    while r < len(t):
+        if t[r] == s[l]:
+            if l == len(s) - 1:
+                return True
+            l += 1
+        r += 1
+    return False
+
+s = "abc"
+t = "ahbgdc"
+
+print(isSubsequence(s,t))
